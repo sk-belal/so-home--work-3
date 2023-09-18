c2b7a934f07fd729a7afc9cc39c56738851f8e23
Sheikh Ahmed Belal <134007028+sk-belal@users.noreply.github.com>
2023-09-19 00:46:51 +0600

    Home work -somajsheba

---
+++ java.js
@@ -0,0 +1,33 @@
+//var score=0;
+//while(score<20){
+    //document.write(score);
+    //score+=2;
+//}
+
+// var score=10;
+// while(score>=1){
+//     document.write(score);
+//     score--;
+// }
+
+ //sum
+// var egg=40;
+// var apple=50;
+// var totalCost=egg+apple
+// document.write(totalCost);
+
+// 
+// var totalMoney=2000;
+// var cost=1260;
+// var netCash=(totalMoney-cost );
+// document.write(netCash);
+
+// var mangoPrice=170;
+// var need=7;
+// var totalCost=(mangoPrice*need );
+// document.write(totalCost);
+
+// var totalMoney=1200;
+// var divided=8;
+// var earn=( totalMoney/divided);
+// document.write(earn);
